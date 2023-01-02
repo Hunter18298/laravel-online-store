3cf833c042498f2a3c25d378c0b486820e5808e8
Hunter18298 <aland.goran182@gmail.com>
2023-01-02 05:10:59 +0300

    cart add

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -1,11 +1,37 @@
 <php?
  namespace App\Http\Controllers;
+
  use App\Models\Product;
  use Illuminate\Http\Request;
 
  class CartController extend Controller
  {
-    
+    public function index(Request $request)
+    {
+        $total=0;
+        $productsInCart=[];
+        $productSession=$request->session()->get('products');
+        if($productSession)
+        {
+            $productsInCart=Products::findMany(array_keys($productSession));
+            $total=Product::sumPricesByQuantity($productsInCart,$productSession);
+        }
+        $viewData = [];
+        $viewData["title"] = "Cart - Online Store";
+        $viewData["subtitle"] = "Shopping Cart";
+        $viewData["total"] = $total;
+        $viewData["products"] = $productsInCart;
+        return view('cart.index')->with("viewData", $viewData);
+
+    }
+
+    public funtion add(Request $request, $id)
+    {
+        $products=$request->session()->get('products');
+        $products['id']=$request->input('quantity');
+        $request->session()->put('products',$products);
+        return redirect()->route('cart.index');
+    }
  }
 
 
